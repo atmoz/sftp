d0a1639d2f89480cba6e58de93038b55d0717a2a
Joshua B <joshdev@9ci.com>
2019-10-11 19:30:45 -0600

    big refactor to permissions

--- examples/dock-run.sh
+++ examples/dock-run.sh
@@ -3,8 +3,11 @@
 # run this from the root project
 
 # docker build -t yakworks/sftp .
+docker stop sftp || true && docker rm sftp || true
 
-docker run --name sftp --rm --cap-add=SYS_ADMIN -p 30022:22 \
+docker run --name sftp --cap-add=SYS_ADMIN \
+  -p 30022:22 \
+  -e DATA_MOUNT_NAME=ninebox \
   -v $(pwd)/examples/users.conf:/etc/sftp/users.conf \
-  -v $(pwd)/examples/sftp-data:/sftp-data \
-  yakworks/sftp
+  -v $(pwd)/examples/sftp-data:/data \
+  -d yakworks/sftp
